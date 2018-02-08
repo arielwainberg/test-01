b66c8b0084e95011408b5c693ee680adbc49ba75
Ariel Wainberg <arielw81@gmail.com>
2018-02-08 16:56:49 +0200

    First question on mission-01-20180208.rtf

---
+++ homework/Arielw-mission-01-20180208.py
@@ -0,0 +1,9 @@
+################################################
+# Mission 1 20180802
+#age = input("how old are you?  ")
+#destiny = 120
+#ttl = destiny - int(age)
+#print(f"You have left {ttl} years to live ")
+################################################
+#
+#Mission 2
